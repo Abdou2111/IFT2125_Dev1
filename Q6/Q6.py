56e09ff731526bbb0172ab63c237b83a7806d5bf
Marc Olivier <kizoung@gmail.com>
2025-03-03 22:27:16 -0500

    Q6 complexity: O(m+n)

--- Q6/Q6.py
+++ Q6/Q6.py
@@ -1,17 +1,49 @@
-# Nom(s) étudiant(s) / Name(s) of student(s):
+# Marc Olivier Jean Paul: 20241763
+# Abdelghafour Rahmouni: 20246224
 
 import sys
 
 # Espace pour fonctions auxillaires :
 # Space for auxilary functions :
 
-
-
+# Cette fonction prend en paramètre deux tableaux d'entiers et retourne
+# un tableau trié contenant les éléments des deux tableaux.
+# Complexité recherchée: O(log(n+m))
+# Complexitée obtenue: O(n+m)
+def tri(nums1, nums2):
+    tab = []                                    # O(1)
+    i = 0                                       # O(1)                 
+    j = 0
+    while i < len(nums1) and j < len(nums2):    # O(n+m)
+        if nums1[i] < nums2[j]:                 # O(1)
+            tab.append(nums1[i])                # O(1)
+            i += 1                              # O(1)
+        else:
+            tab.append(nums2[j])                # O(1)
+            j += 1                              # O(1)
+    while i < len(nums1):                       # O(n)
+        tab.append(nums1[i])                    # O(1)
+        i += 1                                  # O(1)
+    while j < len(nums2):                       # O(m)
+        tab.append(nums2[j])                    # O(1)
+        j += 1                                  # O(1)
+    return tab                                  # O(1)
 
 # Fonction à compléter / function to complete:
-def solve(nums1,nums2):
-    pass
 
+# Cette fonction prend en paramètre deux tableaux d'entiers et retourne
+# la valeur médiane des deux tableaux
+# Complexité recherchée: O(log(n+m))
+# Complexité obtenue: O(n+m)
+def solve(nums1,nums2):
+    num = tri(nums1, nums2)                                     # O(n+m)          
+    # Cas ou la taille du tableau est paire                                       
+    if len(num) % 2 == 0:                                       # O(1)        
+        return (num[len(num)//2] + num[len(num)//2 - 1]) / 2    # O(1)
+    # Cas ou la taille du tableau est impaire
+    else:                                                       # O(1)
+        return num[len(num)//2]                                 # O(1)
+    
 
 # Ne pas modifier le code ci-dessous :
 # Do not modify the code below :
