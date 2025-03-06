935f08e48e393e26946a9241c4e8b34d4035e257
Abdou2111 <abdelghafour.rahmouni@gmail.com>
2025-03-05 19:49:26 -0500

    Q5 corrigé

--- Q5/CareerCalculator.cpp
+++ Q5/CareerCalculator.cpp
@@ -6,15 +6,25 @@ CareerCalculator::CareerCalculator() {
 using namespace std;
 
 bool CareerCalculator::CalculateMaxCareer(const std::vector<int>& steps) {
-
-    int step = 0;
-    bool feasable = true;
-
-    while (feasable && step < steps.size()) {	// O(n) pire cas, si le vecteur contient que des 1
-        step += steps[step];	// On avance de steps[step] pas (le int à l'index 'step' du vecteur 'steps')
-        if (steps[step] == 0) {
-            feasable = false;	// Si on tombe sur un 0, on avance plus
+    int maxReach = 0; // Initialize the maximum reachable index to 0
+    int step = 0; // Initialize the current step to 0
+    bool feasible = true; // Initialize the feasibility to true
+    while (feasible && step < steps.size()) {
+        if (step > maxReach) {
+            // If the current index is greater than the maximum reachable index,
+            // it means we cannot reach this position, so return false
+            feasible = false;
+        } else{
+            // Update the maximum reachable index
+        	maxReach = max(maxReach, step + steps[step]);
+        	if (maxReach >= steps.size() - 1) {
+            	// If the maximum reachable index is greater than or equal to the last index,
+            	// it means we can reach or exceed the last position, so return true
+            	feasible = true;
+        	}
         }
+        step++;
     }
-    return feasable;
+    // If we finish the loop without reaching the last position, return false
+    return feasible;
 }
